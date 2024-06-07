63a0f400084253ceec40d87f0cc599940aac2347
João Lira <joao.liraves07@gmail.com>
2024-06-07 16:57:44 -0300

    conclusão das outras atividades

--- atividade07/atividade07.cpp
+++ atividade07/atividade07.cpp
@@ -7,23 +7,105 @@ using namespace std;
 //salário desse funcionário.A seguir, mostre o número e o salário do
 //funcionário.
 
-int salario(int a, int b) {
-	return a * b;
-}
-
-int main()
-{
-	int funcionarios, horas, valorPorHora;
-	cout << "Qual o seu numero de funcionário: ";
-	cin >> funcionarios;
-	cout << "Digite horas trabalhadas: ";
-	cin >> horas;
-	cout << "Digite o valor recebido por hora: ";
-	cin >> valorPorHora;
-
-	int resultado = salario(horas, valorPorHora);
-	cout << "O funcionario " << funcionarios << " recebe R$" << resultado;
+//int salario(int a, int b) {
+//	return a * b;
+//}
+//
+//int main()
+//{
+//	int funcionarios, horas, valorPorHora;
+//	cout << "Qual o seu numero de funcionário: ";
+//	cin >> funcionarios;
+//	cout << "Digite horas trabalhadas: ";
+//	cin >> horas;
+//	cout << "Digite o valor recebido por hora: ";
+//	cin >> valorPorHora;
+//
+//	int resultado = salario(horas, valorPorHora);
+//	cout << "O funcionario " << funcionarios << " recebe R$" << resultado;
 }
 
 //2 – Fazer um programa para ler, a quantidade e o valor unitário de
 //dois produtos e mostre quanto no final a pessoa deve pagar 
+
+//double total(int a, int b);
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//
+//
+//	int quantidade;
+//	double valor;
+//
+//	cout << "Insira as informações: " << std::endl;
+//	cout << "Quantidade do produto: ";
+//	cin >> quantidade;
+//	cout << "Valor unitário do produto: ";
+//	cin >> valor;
+//
+//	double resultado = total(quantidade, valor);
+//
+//	cout << "Total: " << resultado;
+//}
+//	double total(int a, int b){
+//		return(a * b);
+//}
+
+// 3 - Fazer um programa que leia três valores: A, B e C. Em seguida,
+	//calcule e mostre :
+	//a) a área do triângulo retângulo que tem A por base e C por altura.
+	//Fórmula: A * C / 2.0
+	//	b) a área do círculo de raio C. (pi = 3.14159)
+	//	Fórmula: 3.14159 * C * C
+	//	c) a área do trapézio que tem A e B por bases e C por altura.
+	//	Fórmula: (A + B) / 2.0 * C
+	//	d) a área do quadrado que tem lado B.
+	//	Fórmula: B * B
+	//	e) a área do retângulo que tem lado A e altura B.
+	//	Fórmula: A * B
+
+	//float triangulo(int a, int b, int c);
+	//float circulo(int a,int b, int c);
+	//float trapezio(int a, int b, int c);
+	//float quadrado(int a, int b);
+	//float retangulo(int a, int b);
+
+	//int main() {
+	//	locale::global(locale("pt_BR.UTF-8"));
+
+	//	float valorA;
+	//	float valorB;
+	//	float valorC;
+	//	double pi = 3.14;
+
+	//	cout << "Escreva três valores: " << std::endl;
+	//	cout << "Primeiro Valor: ";
+	//	cin >> valorA;
+	//	cout << "Segundo Valor: ";
+	//	cin >> valorB;
+	//	cout << "Terceiro Valor: ";
+	//	cin >> valorC;
+
+	//	cout << "Resultado: " << std::endl;
+	//	cout << "\n";
+	//	cout << "Triangulo: " << triangulo(valorA, valorB, valorC) << std::endl;
+	//	cout << "Circulo: " << circulo(pi, valorC, valorC) << std::endl;
+	//	cout << "trapezio: " << trapezio(valorA, valorB, valorC) << std::endl;
+	//	cout << "quadrado: " << quadrado(valorB, valorB) << std::endl;
+	//	cout << "retangulo: " << retangulo(valorA, valorB) << std::endl;
+	//}
+
+	//float triangulo(int a, int b, int c){
+	//	return a * c / 2.0;
+	//};
+	//float circulo(int a, int b, int c){
+	//	return a * c * c;
+	//};
+	//float trapezio(int a, int b, int c){
+	//	return (a + b) / 2.0 * c;
+	//};
+	//float quadrado(int a, int b){
+	//	return b * b;
+	//};
+	//float retangulo(int a, int b){
+	//	return a * b;
+	//};
